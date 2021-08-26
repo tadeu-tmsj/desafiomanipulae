11a6482e8729e4db22267f99318e208e1fabc35b
Tadeu Martins <tadeu.tmsj@gmail.com>
2021-08-25 22:11:58 -0300

    Commit Favoritas

--- src/components/tabela.js
+++ src/components/tabela.js
@@ -1,29 +1,50 @@
 import React, { useState } from "react";
-import Icon from '@material-ui/core/Icon';
 import ReactAudioPlayer from 'react-audio-player';
-import api from "../services/api";
 import { listatop } from "../mock_data";
 import { MdFavoriteBorder } from "react-icons/md";
-import { useEffect } from "react";
 
 const Tabela = (props) => {
 const [favoritas, setFavoritas] = useState([])
+const [dadosTabela, setDadosTabela] = useState({
+  nome: 'Top 10 Músicas',
+  dados: listatop.tracks.data,
+  botao: 'Músicas Favoritas'
+})
 
 const favoritar = (data) => {
-  setFavoritas((prevState) => ({
+  const existe = favoritas.find(element => element.id == data.id)
+  if (!!existe) {
+    let temp = favoritas
+    temp.splice(favoritas.indexOf(existe), 1)
+    setFavoritas(temp)
+  } else {
+  setFavoritas((prevState) => ([
     ...prevState,
-    [data.id]: data
-  }))
+    data
+  ]))
+  }
 }
 
-useEffect (()=> {
-  console.log(favoritas)
-}, [favoritas])
+const mudaTabela = () => {
+  if(dadosTabela.nome === 'Top 10 Músicas'){
+    setDadosTabela(() => ({
+      nome : 'Músicas Favoritas',
+      dados : favoritas,
+      botao: 'Top 10 Músicas'
+    }))
+  } else {
+    setDadosTabela(() => ({
+      nome: 'Top 10 Músicas',
+      dados: listatop.tracks.data,
+      botao: 'Músicas Favoritas'
+    }))
+  }
+}
 
   return (
     <div>
       <h1>Lista Top 10 Músicas</h1>
-      <button>Favoritas</button>
+      <button onClick={() => mudaTabela()}>Visualizar {dadosTabela.botao}</button>
       <table>
         <thead>
           <tr>
@@ -33,20 +54,20 @@ useEffect (()=> {
             <th>Duração</th>
             <th>Link</th>
             <th>Preview</th>
-            <th>Favoritar</th>
+            {dadosTabela.nome === 'Top 10 Músicas' && <th>Favoritar</th>}
           </tr>
         </thead>
         <tbody>
-          {listatop.tracks.data.map((musica) => {
+          {dadosTabela.dados.map((musica) => {
             return (
               <tr key={musica.id}>
-                <td><img src={musica.artist.picture_small} /></td>
+                <td><img src={musica.artist.picture_small} alt='Capa do Álbum' /></td>
                 <td>{musica.title}</td>
                 <td>{musica.artist.name}</td>
                 <td>{secToMin(musica.duration)}</td>
                 <td><a target="_self" href={musica.link}>&#127925;</a></td>
                 <td><ReactAudioPlayer source src={musica.preview} controls/></td>
-                <td style={{textAlign: 'center'}} onClick={() => favoritar(musica)}><MdFavoriteBorder /></td>
+                {dadosTabela.nome === 'Top 10 Músicas' && <td style={{textAlign: 'center'}} onClick={() => favoritar(musica)}><MdFavoriteBorder /></td>}
               </tr>
             );
           })}
